8f8029e874421f97edd9438bea0728731445f475
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-12 16:01:20 +0400

    alpha version

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :events
+
+  before_validation :set_name, on: :create
+
+  validates :name, presence: true, length: { maximum: 35 }
+
+  def set_name
+    self.name = "User №#{rand(777)}" if self.name.blank?
+  end
+end

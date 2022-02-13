555714774c201386c89f982b9ae461755a178d9d
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-13 16:24:19 +0400

    some bootstrap styles 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events
+  has_many :events, dependent: :destroy
 
   before_validation :set_name, on: :create
 

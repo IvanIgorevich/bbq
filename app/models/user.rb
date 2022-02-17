b7ebfe4b73e4f9b9b7894277f36b6c8e1914f977
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-17 18:10:13 +0400

    some bootstrap styles 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
 
-  before_validation :set_name, on: :create
+  before_save :set_name
 
   validates :name, presence: true, length: { maximum: 35 }
 

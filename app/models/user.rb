f064ed5e70774b96a13964f68649a1b0a9e9b8df
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-18 17:29:15 +0400

    add delete event

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
 
-  before_save :set_name
-
   validates :name, presence: true, length: { maximum: 35 }
 
-  def set_name
-    self.name = "User №#{rand(777)}" if self.name.blank?
-  end
 end

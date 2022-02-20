cc1f379f11435250094190036bfb761fd54ff393
Ivan I. Zemchikov <zemchikov0@gmail.com>
2022-02-20 16:07:17 +0400

    subscription not for event creator

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:destroy]
 
   def create
-    @new_subscription = @event.subscriptions.build(subscription_params)
+    @new_subscription = @event.subscriptions.build(subscription_params) unless @event.user == current_user
     @new_subscription.user = current_user
 
     if @new_subscription.save
